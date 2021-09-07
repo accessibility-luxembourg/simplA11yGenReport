0155acded2b4c3e956d0f15f5a6a48f4192d3636
Alain Vagner <alain.vagner@sip.etat.lu>
2021-09-07 12:02:32 +0200

    cleanup and test file

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 filename=$(basename -- "$1")
 filename="rapport-${filename%.*}"
-node index.js $1 && npx pagedjs-cli ./out/$filename.html -o ./out/$filename.pdf
+node index.js $1 && npx pagedjs-cli  ./out/$filename.html -o ./out/$filename.pdf  --browserArgs --export-tagged-pdf
+# --outline-tags h1,h2,h3
